3582dcf9c3179826f722109c9a6153be1cc672a2
mike <mickael.leblan59@gmail.com>
2024-12-05 22:36:06 +0100

    Modification de la chaine de connexion pour Docker Mise en place des classes partial pour 2 pages

---
+++ SannoisWorship/SannoisWorship.Client/Pages/Counter.razor.cs
@@ -0,0 +1,11 @@
+namespace SannoisWorship.Client.Pages;
+
+public partial class Counter
+{
+    private int currentCount = 0;
+
+    private void IncrementCount()
+    {
+        currentCount++;
+    }
+}
